4da76a0043c1052d521b6b57853f81e81bc72e21
Convection <mfish1@gmail.com>
2025-05-08 23:20:35 -0500

    Sewers flags convert to account wide

--- barindu/#Apprentice_Udranda.lua
+++ barindu/#Apprentice_Udranda.lua
@@ -36,12 +36,11 @@ local function create_expedition(client, expedition_info)
 end
 
 function event_say(e)
-  local qglobals = eq.get_qglobals(e.other)
-  local has_vxed_access = (qglobals.god_vxed_access and qglobals.god_vxed_access == "1") -- sewers or rondo complete
-  local has_tipt_access = (qglobals.god_tipt_access and qglobals.god_tipt_access == "1") -- has_permanent_vxed
-
-  local sewers_flag = tonumber(eq.get_data(e.other:CharacterID() .. "-god_sewers")) or 0
-  eq.debug(string.format("sewers[%s] vxed_access[%s] tipt_access[%s]", sewers_flag, tostring(has_vxed_access), tostring(has_tipt_access)))
+  local vxed_flag = tonumber(e.other:GetAccountBucket('god.flags.vxed')) or 0
+  local tipt_flag = tonumber(e.other:GetAccountBucket('god.flags.tipt')) or 0
+  local pool_flag = tonumber(e.other:GetAccountBucket('god.flags.pool')) or 0
+  local has_vxed_access = (vxed_flag == 1) -- sewers or rondo complete
+  local has_tipt_access = (tipt_flag == 1) -- has_permanent_vxed
 
   local is_gm = (e.other:Admin() > 80 and e.other:GetGM())
 
@@ -50,22 +49,19 @@ function event_say(e)
     if e.other:HasClass(Class.PALADIN) and e.other:HasItem(69933) then -- Item: Seal of Enic
       e.other:Message(MT.NPCQuestSay, string.format("I heard you released Reiya from his tourture, %s. I have seen muramites gathering in Vxed and I fear this may have to do with Reiya and the creatures responsible. Go there now and investigate, Noble Knight.", e.other:GetCleanName()))
       create_expedition(e.other, paladin_epic)
-    elseif sewers_flag == 4 and not has_tipt_access then -- sewers progression dialogue
+    elseif pool_flag == 2  then -- sewers progression dialogue
       -- note: live dialogue is always here if finished sewers, checking tipt access flag here avoids this live "bug"
       e.other:Message(MT.NPCQuestSay, "Udranda looks around sheepishly.  'Greetings.  A messenger told me you would come.  I believe we owe you our thanks -- though I'm not sure allowing you to face the terror in the mountains is a reward.  I will have my stone worker allow you to pass through, but you must [" .. eq.say_link("take heed") .. "] first and be as quiet as you can.'")
     else
       e.other:Message(MT.NPCQuestSay, "Udranda tells you, 'Greetings.  I have a duty here to stand guard at the mountain pass and allow the Muramites to pass through by moving the rocks with my magic.'  Udranda looks around for anyone listening and whispers to you. 'If you want to go into [" .. eq.say_link("Tipt") .. "] or [" .. eq.say_link("Vxed") .. "] I will have my stone worker open the passage for you.  If you want to progress past the mountains, I would ask that you first prove your worth with High Priest Diru.'")
     end
   elseif e.message:findi("take heed") and not has_tipt_access then -- no longer responds to this after receiving permanent vxed completed flag
-    if sewers_flag == 4 or has_vxed_access then
-      e.other:Message(MT.NPCQuestSay, "Udranda tells you, 'Good.  There are many secrets I can't tell you right now for fear the Muramites might hear them, but you must do as I say and trust in me.  In the mountain pass, Vxed, you must seek out Stonespiritist Ekikoa.  He is my mentor and will help you in your quest to find passage to the peaks.  We have created a very unique magic that we will share with you, but you must speak with him.  It will all be clear to you when you find him.  Tell me when you are [" .. eq.say_link("ready to pass") .. "] through.  I cannot leave a way open for too long.'")
+    if has_vxed_access then
+      e.other:Message(MT.NPCQuestSay, "Udranda tells you, 'Good.  There are many secrets I can't tell you right now for fear the Muramites might hear them, but you must do as I say and trust in me.  In the mountain pass, Vxed, you must seek out Stonespiritist Ekikoa.  He is my mentor and will help you in your quest to find passage to the peaks.  We have created a very unique magic that we will share with you, but you must speak with him.  It will all be clear to you when you find him.  Please hurry, I cannot leave a way open for too long.'")
       create_expedition(e.other, vxed) -- no emote when creating here
     else -- same message as hailing without progression
       e.other:Message(MT.NPCQuestSay, "Udranda tells you, 'Greetings.  I have a duty here to stand guard at the mountain pass and allow the Muramites to pass through by moving the rocks with my magic.'  Udranda looks around for anyone listening and whispers to you. 'If you want to go into [" .. eq.say_link("Tipt") .. "] or [" .. eq.say_link("Vxed") .. "] I will have my stone worker open the passage for you.  If you want to progress past the mountains, I would ask that you first prove your worth with High Priest Diru.'")
     end
-  elseif e.message:findi("ready to pass") and not has_tipt_access then
-    -- only responds without permanent vxed flag, possibly old dialogue to request tipt that was removed
-    e.other:Message(MT.NPCQuestSay, "Udranda tells you, 'I cannot allow you to pass with your companions.  You seem heroic enough, but you must prove your worth to High Priest Diru and our stonespiritist in the Vxed mountain pass before I can allow you through.'")
   elseif e.message:findi("tipt") and (is_gm or has_tipt_access) then
     eq.get_entity_list():MessageClose(e.self, true, 100, MT.SayEcho, e.self:GetCleanName() .. " subtly commands her stone worker to open the passage for you before any invaders can take notice.")
     create_expedition(e.other, tipt)
