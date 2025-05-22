6a6fb76b4c8846c6452954dab056a29acb40ada8
Convection <mfish1@gmail.com>
2025-05-21 22:39:08 -0500

    Qinimi

--- abysmal/#Taminoa_Bialu.lua
+++ abysmal/#Taminoa_Bialu.lua
@@ -1,16 +1,57 @@
+-- Task ID definitions
+local bic_abysmal = 9
+local bic_qinimi = 8
+local bic_barindu = 10
+local bic_riwwi = 11
+local bic_ferubi = 12
+local bic_sewers = 13
+local bic_vxed = 14
+local bic_tipt = 15
+local bic_outer = 16
+local bic_yxtta = 17
+local bic_kodtaz = 18
+
 function event_say(e)
-  if e.message:findi('qinimi') then
-    e.other:AssignTask(8)
+  if e.other:IsTaskCompleted(bic_abysmal) or (e.other:IsTaskCompleted(bic_qinimi) and e.other:IsTaskCompleted(bic_barindu) and e.other:IsTaskCompleted(bic_riwwi) and e.other:IsTaskCompleted(bic_ferubi)) then
+    e.other:Message(MT.NPCQuestSay, "Taminoa Bialu says 'Thank you for all of your help, " .. e.other:GetCleanName() .. ".  I've reported your activities to Fezbin and he is eager to speak to you once you've helped the other scout masters.'")
+  elseif not e.other:IsTaskActive(bic_abysmal) then
+    e.other:Message(MT.NPCQuestSay, "Tamina Bialu says 'I don't mean to be rude, but I am really busy at the moment. Unless you have something important to tell me, I must ask that you leave me be.'")
+  else
+    if e.message:findi('hail') then
+      e.other:Message(MT.NPCQuestSay, "Taminoa Bialu says 'Greetings. You must be one of the adventurers that Fezbin told me about. It is a pleasure to meet such a brave individual. Where are my manners? My name is Taminoa Bialu and I compile all the information from our scouts in the [" .. eq.say_link('city') .. "] area of Taelosia.'")
+    end
+    if e.message:findi('city') then
+      e.other:Message(MT.NPCQuestSay, "Taminoa Bialu says 'Well, it appears the native inhabitants of Taelosia lived in a grand city once. This city is made up of four different areas named in their language. They are [" .. eq.say_link('Qinimi') .. "], [" .. eq.say_link('Riwwi') .. "], [" .. eq.say_link('Barindu') .. "], and [" .. eq.say_link('Ferubi') .. "]. Based off of early information, we believe each area had a specific purpose. I am still trying to decipher the native language to figure out what the names mean, but going off of visual information I can assume that Qinimi served as their court, Riwwi is where they held games, Barindu was a lush garden, and Ferubi was a central place of worship once. That is about the extent of my information as far as the intended purpose of each area. What they are now used for I can explain in further detail. Which area would you like to hear more about?'")
+    end
+    if e.message:findi('qinimi') and not e.other:IsTaskActive(bic_qinimi) then
+      e.other:Message(MT.NPCQuestSay, "Taminoa Bialu says 'It appears the Muramites have made this some sort of stronghold. Destroying many of the native symbols, the invaders have converted the building that once served as the court into fortified headquarters. It is here you will find a structure called the Chamber of Souls, various pens to hold ukun, and a moat of sickly black sludge. The structures that the nihil once called home are all but destroyed now. Very sad indeed. That's where the legion keeps the bulk of the [" .. eq.say_link('slaves') .. "]. I believe that the scout for this area was caught sneaking around the housing district, but I have no proof. If you were to search this area, maybe you would find some sort of sign or marker he may have left. That would be helpful.'")
+      e.other:AssignTask(bic_qinimi)
+    end
+    if e.message:findi('slaves') then
+      e.other:Message(MT.NPCQuestSay, "Taminoa Bialu says 'I see Fezbin saved some of the more dismal news for me. The legion has enslaved the natives of the continent. In the city area, these slaves are used mainly for menial tasks. Due to their fragile bodies, they cannot do any heavy lifting, but their knowledge of shaping stone makes them perfect for building new structures for the legion to use as well as altering current ones. They command the stone workers. I am not sure how the slaves are used in the temple areas. That information is held by L`diava.'")
+    end
   end
 end
 
 function event_trade(e)
   local item_lib = require("items")
 
-  if item_lib.check_turn_in(e.trade, {item1 = 67397}) then
+  if item_lib.check_turn_in(e.trade, {item1 = 67397}) then --Kreshin's Journal Page
     e.other:Message(MT.NPCQuestSay, "Taminoa Bialu says 'Yes. This is exactly what I needed. Excellent job. Okay, now give me one moment to decipher this . . . hm. It seems that just before he was captured he was trying to communicate with the slaves in the area. The text is very hard to make out, but it looks like he may have been trying to get into the main headquarters. He was interacting with a particular slave named Sislono Nislan. This slave promised him a way into the headquarters only if he promised to rid the area of a tyrant, named Tixxrt. The rest of the parchment is undecipherable. I would suggest finding this slave and showing him this to see if he knows what happened.'")
-    e.other:UpdateTaskActivity(8, 1, 1) --Update task 8, activity 1
+    e.other:UpdateTaskActivity(bic_qinimi, 1, 1)
     e.other:SummonItem(67700)
+  end
+
+  if item_lib.check_turn_in(e.trade, {item1 = 67401}) then --Writ of the Magi
+    e.other:Message(MT.NPCQuestSay, "Taminoa Bialu says 'Finally, you have returned and with some information I see. Kreshin contacted me in advance to let me know of your daring rescue. We are indebted to you. Now, back to business. While I cannot make out most of it, Kreshin has translated some of it and it appears to be a description of a ritual one of the head ikaav performs regularly. Apparently she has been trying to absorb the geomantic powers of the natives of Taelosia. The details are kind of sketchy as Kreshin was only able to translate so much, but some information is better than none, I always say. Anyway, it says here that the ritual is conducted in the Chamber of Souls. It used to be a large tower which the natives used to watch the shores. The tower was destroyed when the invaders took over the area and converted it to a ritual chamber. They used it in experiments centered on the extraction of magic. The head ikaav, known as Xictic, has been trying to use the powers of the native Taelosians to build an army of stone workers that he can directly control instead of relying on the ability of slaves. During the ritual, Xictic stands atop a platform which is suspended over a circle of platforms. The center platform holds an inanimate stone worker while eight ikaav stand on smaller platforms around the center. What happens next is not really known, but the end result is three lifeless slave corpses. According to this, she has not yet been successful, which I would say is good news. But if we do not stop her, she could easily reinforce the numbers with stone workers tainted by the touch of this legion. I know you have accomplished your goal, but I must ask that you find a way into this ritual and silence Xictic for good. Show this to Nislan. He should be able to decipher the images. I believe this is the key to getting into the ritual chamber. Now, hurry please and return to me with proof of Xictic's death so that I can make a final report on the Qinimi area to Fezbin.'")
+    e.other:UpdateTaskActivity(bic_qinimi, 9, 1)
+    e.other:SummonItem(67403)
+  end
+
+  if item_lib.check_turn_in(e.trade, {item1 = 67511}) then --Xictic's Bloody Robes
+    e.other:Message(MT.NPCQuestSay, "Taminoa Bialu says 'You have returned bruised, but not broken I see. I am disappointed to hear that he got away, but you are not at fault. We could not foresee how crafty these beings would be. The information you have given us will be invaluable. Fezbin will be pleased. Kreshin told me to give this to you. He believes it may be one of the fragments that will fit into the stone Fezbin gave you. Who knows, if you help me find the rest of our scouts we may yet find more fragments.'")
+    e.other:UpdateTaskActivity(bic_qinimi, 13, 1)
+    e.other:UpdateTaskActivity(bic_abysmal, 0, 1)
   end
 
   item_lib.return_items(e.self, e.other, e.trade)
--- qinimi/#Councilman_Sislono_Nislan.lua
+++ qinimi/#Councilman_Sislono_Nislan.lua
@@ -7,6 +7,19 @@ local thunder_dome_id;
 local raid_id_by_thunder_dome	= {};
 local zone_id					= eq.get_zone_id();
 
+-- Task ID definitions
+local bic_abysmal = 9
+local bic_qinimi = 8
+local bic_barindu = 10
+local bic_riwwi = 11
+local bic_ferubi = 12
+local bic_sewers = 13
+local bic_vxed = 14
+local bic_tipt = 15
+local bic_outer = 16
+local bic_yxtta = 17
+local bic_kodtaz = 18
+
 function event_say(e)
 	local qglobals = eq.get_qglobals(e.self, e.other);
 	local instance_id = eq.get_zone_instance_id();
@@ -18,7 +31,7 @@ function event_say(e)
 	local x = e.self:GetX();
 	local y = e.self:GetY();
 
-	if qglobals["bic_qin"] ~= nil and qglobals["bic_qin"] == "3" and x ~= -640 and y ~= 257 then
+	if e.other:IsTaskActivityActive(bic_qinimi, 10) and x ~= -640 and y ~= 257 then
 		if e.message:findi("depart") then
 			e.other:Message(MT.NPCQuestSay, "Councilman Sislono Nislan nods his head and focuses on the small piece of rock.  In a flash, you are transported back to his hut.")
 			local instance_id = eq.get_zone_instance_id();
@@ -29,19 +42,19 @@ function event_say(e)
 				e.other:MovePCInstance(zone_id, instance_id, unpack(hut_locs));
 			end
 		elseif e.message:findi("hail") then
-			e.other:Message(MT.NPCQuestSay, "Councilman Sislono Nislan says, 'Amazing!  I did not believe that anyone could stand up to the commanders of these muramite forces and live to tell the tale!  I hope you found something useful in your search of her body.  If you are ready to [depart] this place, please just say so.'");
+			e.other:Message(MT.NPCQuestSay, "Councilman Sislono Nislan says, 'Amazing!  I did not believe that anyone could stand up to the commanders of these muramite forces and live to tell the tale!  I hope you found something useful in your search of her body.  If you are ready to [" .. eq.say_link("depart") .. "] this place, please just say so.'");
 		end
-
-	elseif qglobals["bic_qin"] ~= nil and qglobals["bic_qin"] > "2" then
+  elseif e.other:IsTaskActivityActive(bic_qinimi, 11) or e.other:IsTaskActivityActive(bic_qinimi, 12) then
 		if e.message:findi("ritual") then
-			e.other:Message(MT.NPCQuestSay, "While animating stone comes easy for us, sometimes we need an extremely powerful stone worker to help with the city. These stone servants were made through a ritual which combined the power of eight geomancers into a ninth. This geomancer would then imbue a stone worker with this power, resulting in a more powerful construct. We stopped doing the ritual when we discovered that it could take away our ability over time. If [she] has somehow figured out a way to absorb this power, then my people could be in more danger than I thought possible.'");
+			e.other:Message(MT.NPCQuestSay, "While animating stone comes easy for us, sometimes we need an extremely powerful stone worker to help with the city. These stone servants were made through a ritual which combined the power of eight geomancers into a ninth. This geomancer would then imbue a stone worker with this power, resulting in a more powerful construct. We stopped doing the ritual when we discovered that it could take away our ability over time. If [" .. eq.say_link('she') .. "] has somehow figured out a way to absorb this power, then my people could be in more danger than I thought possible.'");
 		elseif e.message:findi("she") then
-			e.other:Message(MT.NPCQuestSay, "We refer to her as the silent one, but the legion calls her Xictic. She is one of their kind who can use magic and she seems to be quite feared by everyone in this area. She comes here sometimes and takes three of us into the building with the glowing dome around it. Along with her are eight magic-using invaders and some of the slavers. When they are finished, only members of the legion leave. None of my people who enter are ever seen again. If what this says is true, you must stop her. If she succeeds, she will be able to control our stone servants at will and cause even more destruction. I beg you to stop her. I will help you in any way if you [promise] to stop her.'");
+			e.other:Message(MT.NPCQuestSay, "We refer to her as the silent one, but the legion calls her Xictic. She is one of their kind who can use magic and she seems to be quite feared by everyone in this area. She comes here sometimes and takes three of us into the building with the glowing dome around it. Along with her are eight magic-using invaders and some of the slavers. When they are finished, only members of the legion leave. None of my people who enter are ever seen again. If what this says is true, you must stop her. If she succeeds, she will be able to control our stone servants at will and cause even more destruction. I beg you to stop her. I will help you in any way if you [" .. eq.say_link('promise') .. "] to stop her.'");
 		elseif e.message:findi("promise") then
-			e.other:Message(MT.NPCQuestSay, "I cannot tell you how relieved this makes me. Now to the issue at hand. While Xictic and those she chooses can roam freely in and out of the magical dome surrounding the chamber, if anyone else tries to enter they are held back. As a councilman, I was granted a device that allows access into the dome and I've managed to keep it away from the prying eyes of the Mata Muram. Using it is not without its consequences, though. As soon as an outsider shows up within the dome, they will know what has occurred and will come searching for the culprit so be certain you are ready to face the challenges ahead before you embark upon this venture. Only eighteen of you will be allowed into the chamber at one time so gather your forces and tell me you are [ready] to face Xictic.'");
-		elseif e.message:findi("ready") then
+			e.other:Message(MT.NPCQuestSay, "I cannot tell you how relieved this makes me. Now to the issue at hand. While Xictic and those she chooses can roam freely in and out of the magical dome surrounding the chamber, if anyone else tries to enter they are held back. As a councilman, I was granted a device that allows access into the dome and I've managed to keep it away from the prying eyes of the Mata Muram. Using it is not without its consequences, though. As soon as an outsider shows up within the dome, they will know what has occurred and will come searching for the culprit so be certain you are ready to face the challenges ahead before you embark upon this venture. Only eighteen of you will be allowed into the chamber at one time so gather your forces and tell me you are [" .. eq.say_link('ready') .. "] to face Xictic.'");
+      e.other:UpdateTaskActivity(bic_qinimi, 11, 1)
+    end
+	  if e.message:findi("ready") then
 			e.self:Emote("pulls out a small stone and closes it in his hand. 'Please be careful. What you are about to see may shock you at first, but don't let yourself be distracted for too long. These beings are merciless and once they have discovered your presence, they will stop at nothing to add your corpse to the others in the area.'");
-
 			if qglobals["gates_thunder_dome_event_1"] == nil and event_up_1.valid and raid_id_by_thunder_dome[1] == nil then
 				raid_id_by_thunder_dome[1] = raid_id;	-- assign new raid id
 				thunder_dome_id = 1;					-- thunder_dome_id 1 available
@@ -54,6 +67,7 @@ function event_say(e)
 			else
 				thunder_dome_id = -1;					-- no thunder_dome_ids available
 			end
+
 			if thunder_dome_id > 0 and thunder_dome_id < 4 and raid_id_by_thunder_dome[thunder_dome_id] == raid:GetID() then
 				local instance_id = eq.get_zone_instance_id();
 				local event_group = e.other:GetGroup();
@@ -68,14 +82,15 @@ function event_say(e)
 				e.self:Say("Heroes, it is too dangerous to confront Xictic at this time. You must wait until a more opportune moment to strike.");
 			end
 		end
-	elseif qglobals["bic_qin"] ~= nil and qglobals["bic_qin"] == "2" then
+  elseif e.other:IsTaskActivityActive(bic_qinimi, 6) then
 		if e.message:findi("thing") then
 			e.other:Message(MT.NPCQuestSay, "Councilman Sislono Nislan says, 'The geomantic device which raises you into the courtroom is an ancient one and takes time to reset after it has been used. If you are having trouble entering, be patient. You will know it is ready to receive a key when you hear a grinding sound from the base of the device as it settles into place. Please be sure you are fully prepared before entering. While it once was a place of peaceful discussion, the courtroom is now a place where terrible torture is conducted by those the legion wants information from. When they see uninvited guests, they will not be very happy. The leader of this area's force is known as Tixxrt and stopping him is the only way you can save the small one, called Kreshin.'");
-		elseif e.message:findi("stone of entry") then
-			e.other:Message(MT.NPCQuestSay, "Councilman Sislono Nislan says, 'Gaining access to such a private place has always been reserved for the elders of my people. While the legion may have destroyed our home, they have yet to figure out a way around many of our wards that bar access to our sacred areas. Unfortunately, their frustration at gaining entry resulted in the painful torture of many of my people until they were given the keys to enter. The courtroom requires one of these keys and while the invaders hold all of the existing keys, I can create one if you can retrieve the proper [items].'");
+    end
+	elseif e.other:IsTaskActivityActive(bic_qinimi, 4) then
+		if e.message:findi("stone of entry") then
+			e.other:Message(MT.NPCQuestSay, "Councilman Sislono Nislan says, 'Gaining access to such a private place has always been reserved for the elders of my people. While the legion may have destroyed our home, they have yet to figure out a way around many of our wards that bar access to our sacred areas. Unfortunately, their frustration at gaining entry resulted in the painful torture of many of my people until they were given the keys to enter. The courtroom requires one of these keys and while the invaders hold all of the existing keys, I can create one if you can retrieve the proper [" .. eq.say_link('items') .. "].'");
 		elseif e.message:findi("items") then
 			e.other:Message(MT.NPCQuestSay, "Councilman Sislono Nislan says, 'The key consists of mud, some stonedust particles, and a piece of chalk. The mud can be found in the sewers beneth the city. The stonedust is plentiful near the coliseum, and the chalk can be found all over this area. Bring these to me and I will make you a temporary key which will grant you access to the courtroom. I implore you to hurry. I fear the one you seek may not be alive much longer.'");
-
 		end
 	else
 		if e.message:findi("hail") then
@@ -94,19 +109,35 @@ function event_signal(e)
 end
 
 function event_trade(e)
-	local qglobals = eq.get_qglobals(e.self, e.other);
 	local item_lib = require("items");
-	if qglobals["bic"] ~= nil and qglobals["bic"] >= "1" and item_lib.check_turn_in(e.trade, {item1 = 67700}) then -- Item: Kreshin's Journal Page
-		e.other:Message(MT.NPCQuestSay, "Councilman Sislono Nislan says, 'Yes, I have seen this and others like it before. While we do not understand where it comes from, we have come to the conclusion that these strange glyphs express somthing important to your people. These particular glyphs were made by the small one they hold captive in the courtroom. If you wish to help him, you will have to retrieve the [stone of entry]. Only with this stone can you enter the courtroom and help the one who calls himself Kreshin.");
-		eq.set_global("bic_qin","2",5,"F");
-	elseif qglobals["bic_qin"] ~= nil and qglobals["bic_qin"] == "2" and item_lib.check_turn_in(e.trade, {item1 = 67398,item2 = 67399,item3 = 67400}) then -- Item: Stonedust Particles, Glob of Mud, Piece of Chalk
-		e.other:Message(MT.NPCQuestSay, "Sislono covers the three pieces in his hands, closes his eyes, and begins to chant. A glow begins to emanate from his hands and he begins to chant louder. Then, just as quickly as it began, the glow in his hands dims and the chanting stops. 'There you are -- the key which grants you and your party access to the courtroom. Just stand on the pedestal in the center of the large building in this part of the city with the key in your hand and say you wish to enter. There is one more [thing] I must tell you before you go.'");
-		e.other:QuestReward(e.self,0,0,0,0,67415,eq.ExpHelper(60)); -- Item: Stone of Entry
-	elseif item_lib.check_turn_in(e.trade, {item1 = 67403}) then -- Item: Stonedust Particles, Glob of Mud, Piece of Chalk
-		e.other:Message(MT.NPCQuestSay, "These glyphs look quite familiar. They are definitely in my language, but it seems as if the one who etched them did so against his will. Where did you get this? Wait, don't tell me. I know. Kreshin must have been successful in finding out what was going on in the Chamber of Souls. These glyphs detail how to animate stone using our geomancy abilities. There are some strange symbols toward the bottom, but for the most part, this is our language. Could he be trying to conduct the [ritual] of my people?'");
-		if qglobals["bic_qin"] ~= nil and qglobals["bic_qin"] < "3" then
-			eq.set_global("bic_qin","3",5,"F");
-		end
+	if item_lib.check_turn_in(e.trade, {item1 = 67700}) then -- Item: Kreshin's Journal Page
+    eq.debug("Yes")
+		e.other:Message(MT.NPCQuestSay, "Councilman Sislono Nislan says, 'Yes, I have seen this and others like it before. While we do not understand where it comes from, we have come to the conclusion that these strange glyphs express somthing important to your people. These particular glyphs were made by the small one they hold captive in the courtroom. If you wish to help him, you will have to retrieve the [" .. eq.say_link('stone of entry') .. "]. Only with this stone can you enter the courtroom and help the one who calls himself Kreshin.");
+    e.other:UpdateTaskActivity(bic_qinimi, 2, 1)
+  elseif item_lib.check_turn_in(e.trade, {item1 = 67398}) then --Stonedust Particles
+    e.other:UpdateTaskActivity(bic_qinimi, 5, 1)
+    if not e.other:IsTaskActivityActive(bic_qinimi, 3) and not e.other:IsTaskActivityActive(bic_qinimi, 4) and not e.other:IsTaskActivityActive(bic_qinimi, 5) then
+      give_stone(e)
+    end
+  elseif item_lib.check_turn_in(e.trade, {item1 = 67399}) then --Glob of Mud
+    e.other:UpdateTaskActivity(bic_qinimi, 4, 1)
+    if not e.other:IsTaskActivityActive(bic_qinimi, 3) and not e.other:IsTaskActivityActive(bic_qinimi, 4) and not e.other:IsTaskActivityActive(bic_qinimi, 5) then
+      give_stone(e)
+    end
+	elseif item_lib.check_turn_in(e.trade, {item1 = 67400}) then -- Piece of Chalk
+    e.other:UpdateTaskActivity(bic_qinimi, 3, 1)
+    if not e.other:IsTaskActivityActive(bic_qinimi, 3) and not e.other:IsTaskActivityActive(bic_qinimi, 4) and not e.other:IsTaskActivityActive(bic_qinimi, 5) then
+      give_stone(e)
+    end
+	elseif item_lib.check_turn_in(e.trade, {item1 = 67403}) then -- Writ of the Magi
+		e.other:Message(MT.NPCQuestSay, "These glyphs look quite familiar. They are definitely in my language, but it seems as if the one who etched them did so against his will. Where did you get this? Wait, don't tell me. I know. Kreshin must have been successful in finding out what was going on in the Chamber of Souls. These glyphs detail how to animate stone using our geomancy abilities. There are some strange symbols toward the bottom, but for the most part, this is our language. Could he be trying to conduct the [" .. eq.say_link('ritual') .. "] of my people?'");
+    e.other:UpdateTaskActivity(bic_qinimi, 10, 1)
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
 end
+
+function give_stone(e)
+  e.other:Message(MT.NPCQuestSay, "Sislono covers the three pieces in his hands, closes his eyes, and begins to chant. A glow begins to emanate from his hands and he begins to chant louder. Then, just as quickly as it began, the glow in his hands dims and the chanting stops. 'There you are -- the key which grants you and your party access to the courtroom. Just stand on the pedestal in the center of the large building in this part of the city with the key in your hand and say you wish to enter. There is one more [" .. eq.say_link('thing') .. "] I must tell you before you go.'");
+  e.other:SummonItem(67415)
+end
+
