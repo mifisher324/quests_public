2c275ab08c03ab7952d782e159b6da397544d995
Convection <mfish1@gmail.com>
2025-05-21 23:11:38 -0500

    Fix task hard coding

--- qinimi/player.lua
+++ qinimi/player.lua
@@ -1,3 +1,16 @@
+-- Task ID definitions
+local bic_abysmal = 9
+local bic_qinimi = 8
+local bic_barindu = 10
+local bic_riwwi = 11
+local bic_ferubi = 12
+local bic_sewers = 13
+local bic_vxed = 14
+local bic_tipt = 15
+local bic_outer = 16
+local bic_yxtta = 17
+local bic_kodtaz = 18
+
 function event_loot(e)
 	if(e.self:HasClass(Class.DRUID) and e.item:GetID() == 62870) then
 		local qglobals = eq.get_qglobals(e.self);
@@ -14,6 +27,6 @@ end
 
 function event_player_pickup(e)
   if e.item:GetID() == 67397 then
-    e.self:UpdateTaskActivity(8, 0, 1) --Update task 8, activity 0
+    e.self:UpdateTaskActivity(bic_qinimi, 0, 1)
   end
 end
