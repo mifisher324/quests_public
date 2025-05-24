67261cf654da86b8c06b065629c7b094fedf5ac1
Convection <mfish1@gmail.com>
2025-05-23 21:22:57 -0500

    KT Flag

---
+++ natimbi/player.lua
@@ -0,0 +1,12 @@
+local kt_flag = tonumber(e.self:GetAccountBucket("god.flags.kt")) or 0
+
+function event_click_door(e)
+  local door_id = e.door:GetDoorID();
+  if door_id == 1 then
+    if kt_flag == 1 then
+      if not e.self:HasZoneFlag(293) then
+        e.self:SetZoneFlag(293)
+      end
+    end
+  end
+end
